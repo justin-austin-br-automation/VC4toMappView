6023b88f132865682c239505aad142c5f6a76260
austin_justin <justin.austin@br-automation.com>
2023-11-22 15:44:20 -0600

    Files are now created in the correct locations

--- SourceCode/Converter_REV0/VC42MV.py
+++ SourceCode/Converter_REV0/VC42MV.py
@@ -1,7 +1,5 @@
 # C:\VC4toMappView\VC4toMappView\Logical\VisuVC4\Pages\PageEmpty.page
 from functions import *
-from numpy import genfromtxt
-import csv
 
 
 class VC4Page:
@@ -14,7 +12,7 @@ class VC4Page:
         self.mVContentID = self.pageData['PageName'] + "Content"
         self.mVPageID = self.pageData['PageName'] + "Page"
 
-        # manually creating lookup table and widget translation dictionary
+        # manually creating lookup table and widget translation dictionary (this will be changed later)
         self.lookupTable = {"0x00001004": {"0": "Label"},
                                 "0x00001002": {"0x0000016B": "MomentaryPushButton"}}
         self.widgetTranslations = {"Label": {"top": "Top",
@@ -31,10 +29,11 @@ class VC4Page:
                                                 "text": "text"}}
 
     # creates a mappView layout from VC4 data
-    def createMVLayout(self):
+    def createMVLayout(self, path):
         ET.register_namespace('ldef', "http://www.br-automation.com/iat2015/layoutDefinition/v2")
         
         # grab the tree from the xml doc
+        os.chdir(os.path.dirname(__file__))
         tree = ET.parse('TemplateLayout.layout')
 
         # get the root of the tree
@@ -49,14 +48,17 @@ class VC4Page:
         root[0][0].set('height', self.pageData['Height'])
         root[0][0].set('width', self.pageData['Width'])
 
+        path = path + '\Layouts'
+        os.chdir(path)
         tree.write(self.mVLayoutID + '.layout', xml_declaration=True, encoding='utf-8')
 
     # creates a mappView content file from VC4 data
-    def createMVContent(self):
+    def createMVContent(self, path):
         ET.register_namespace('', "http://www.br-automation.com/iat2015/contentDefinition/v2")
-        ET.register_namespace('pdef', "http://www.br-automation.com/iat2015/pageDefinition/v2")
+        ET.register_namespace('xsi', "http://www.w3.org/2001/XMLSchema-instance")
         
         # grab the tree from the xml doc
+        os.chdir(os.path.dirname(__file__))
         tree = ET.parse('TemplateContent.content')
 
         # get the root of the tree
@@ -75,6 +77,10 @@ class VC4Page:
 
         root.set('xmlns:xsi', "http://www.w3.org/2001/XMLSchema-instance")
 
+        path = path + '\Pages\\' + self.mVPageID
+        if not os.path.exists(path):
+            os.makedirs(path)
+        os.chdir(path)
         tree.write(self.mVContentID + '.content', xml_declaration=True, encoding='utf-8')
 
         # editing output file to be usable by AS
@@ -104,11 +110,14 @@ class VC4Page:
             else:
                 attrib = widgetTranslation[attribName]
                 attribValue = component[attrib]
-                widget.set(attribName, attribValue)
+                widget.set(attribName, str(attribValue))
 
     # creates a mappView page from VC4 data
-    def createMVPage(self):
+    def createMVPage(self, path):
+        ET.register_namespace('pdef', 'http://www.br-automation.com/iat2015/pageDefinition/v2')
+
         # grab the tree from the xml doc
+        os.chdir(os.path.dirname(__file__))
         tree = ET.parse('TemplatePage.page')
 
         # get the root of the tree
@@ -121,4 +130,6 @@ class VC4Page:
         # set all Assignment values
         root[0][0].set('baseContentRefId', self.mVContentID)
 
+        path = path + '\Pages\\' + self.mVPageID
+        os.chdir(path)
         tree.write(self.mVPageID + '.page', xml_declaration=True, encoding='utf-8')
--- SourceCode/Converter_REV0/main.py
+++ SourceCode/Converter_REV0/main.py
@@ -1,14 +1,14 @@
 from VC42MV import *
 
 if __name__ == "__main__":
-        
     # !!!!CHANGE THIS TO YOUR PATH!!!!!
-    # path = 'C:\projects\\4.11\mappViewTest\mappViewTest\Logical\Visu\Pages\PageEmpty.page'
-    # page = VC4Page(path)
+    path = 'C:\projects\\4.11\mappViewTest\mappViewTest\Logical\Visu\Pages\PageEmpty.page'
+    page = VC4Page(path)
 
-    # page.createMVLayout()
-    # page.createMVContent()
-    # page.createMVPage()
+    path = "C:\projects\\4.11\mappViewTest\mappViewTest\Logical\mappView\Visualization"
+    page.createMVLayout(path)
+    page.createMVContent(path)
+    page.createMVPage(path)
 
     path = Path("C:\projects\\4.11\mappViewTest\mappViewTest\Logical\mappView\Visualization")
     PackageCreator(path)
